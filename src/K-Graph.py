45e1716e43e6a522301ed7df32c4ab12ffc9b641
Omar Sanchez <Osanchez@umass.edu>
2018-11-06 22:42:30 -0500

    added some useful resources to kgraph.py

--- src/K-Graph.py
+++ src/K-Graph.py
@@ -1,1 +1,16 @@
-print("hello world")
+# https://github.com/thunlp/KB2E
+
+# learning transE resources
+# https://isaacchanghau.github.io/post/transx/
+# https://www.utc.fr/~bordesan/dokuwiki/_media/en/transe_nips13.pdf
+
+# (h, l, t) head, label, tail
+# each of which indicates that there exists a relationship of
+# name label between the entities head and tail
+
+
+class KGraph:
+
+    def learn_trans_e(self, training_set, ):
+        pass
+
